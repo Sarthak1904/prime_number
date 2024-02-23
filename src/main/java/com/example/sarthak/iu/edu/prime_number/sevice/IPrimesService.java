f2288e417d0064cead1f5379e550daac8486b3d5
Sarthak1904 <csarthak051@gmail.com>
2024-02-22 20:49:07 -0500

    draft

---
+++ src/main/java/com/example/sarthak/iu/edu/prime_number/sevice/IPrimesService.java
@@ -0,0 +1,8 @@
+package com.example.sarthak.iu.edu.prime_number.sevice;
+
+public interface IPrimesService {
+
+    boolean isPrime(int n);
+
+    boolean isPrime(long n);
+}
---
+++ src/test/java/com/example/sarthak/iu/edu/prime_number/PrimeNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sarthak.iu.edu.prime_number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
